8882f4d21774556d11523dc56d4e8f13c4a77a76
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-05-08 14:02:53 -0300

    tipos de dados e operadores

---
+++ 1_tipos_de_dados_&_operadores/script.js
@@ -0,0 +1,40 @@
+//String
+console.log('---String---');
+console.log("string 1");
+console.log('string 2');
+console.log(`string 3`);
+
+//Number
+console.log('---Number---');
+console.log(19);
+console.log(1.9);
+console.log(-19);
+console.log(NaN);
+console.log(Infinity);
+
+//Boolean
+console.log('---Boolean---');
+console.log(true);
+console.log(false);
+
+//Empty
+console.log('---Empty---');
+console.log(null);
+console.log(undefined);
+
+//Operadores aritméticos
+console.log('---Operadores aritméticos---');
+console.log(2 > 1);
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(2 <= 1);
+console.log(1 == 1);
+console.log(1 === '1');
+console.log(1 != 2);
+console.log(1 !== '1');
+
+//Operadores lógicos
+console.log('---Operadores lógicos---');
+console.log(true && true);
+console.log(true || false);
+console.log(!true);
